5ce29923b19d93ec5da3ac3ce1de47e3bf79cd9f
Mazen Kotb <mazenkotb@gmail.com>
2017-11-19 15:12:27 -0800

    Add 'support' for Albums (as far as Telegram allows)

--- jtelegrambotapi-core/src/main/java/io/jtelegram/api/message/impl/PhotoMessage.java
+++ jtelegrambotapi-core/src/main/java/io/jtelegram/api/message/impl/PhotoMessage.java
@@ -12,6 +12,14 @@ import java.util.List;
 @ToString
 public class PhotoMessage extends CaptionableMessage<List<PhotoSize>> {
     private List<PhotoSize> photo = new ArrayList<>();
+    /**
+     * This field does not exist in the Telegram Docs, however is
+     * non-zero whenever a photo is a fragment of an album. Albums
+     * are collection of images and videos that have a max size of
+     * 10. The Telegram Bot has no evident way of checking when it
+     * has received the whole album, so do with this as you will
+     */
+    private long mediaGroupId;
 
     @Override
     public List<PhotoSize> getContent() {
--- jtelegrambotapi-core/src/main/java/io/jtelegram/api/message/impl/VideoMessage.java
+++ jtelegrambotapi-core/src/main/java/io/jtelegram/api/message/impl/VideoMessage.java
@@ -9,6 +9,14 @@ import lombok.ToString;
 @ToString
 public class VideoMessage extends CaptionableMessage<Video> {
     private Video video;
+    /**
+     * This field does not exist in the Telegram Docs, however is
+     * non-zero whenever a photo is a fragment of an album. Albums
+     * are collection of images and videos that have a max size of
+     * 10. The Telegram Bot has no evident way of checking when it
+     * has received the whole album, so do with this as you will
+     */
+    private long mediaGroupId;
 
     @Override
     public Video getContent() {

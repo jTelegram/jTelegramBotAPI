a35c81a68842f51e7ee695c0c13f2bbc0c902379
Mazen Kotb <mazenkotb@gmail.com>
2017-11-18 20:31:29 -0800

    Implement and test edit/update events

--- jtelegrambotapi-core/src/main/java/io/jtelegram/api/update/PollingUpdateThread.java
+++ jtelegrambotapi-core/src/main/java/io/jtelegram/api/update/PollingUpdateThread.java
@@ -2,10 +2,18 @@ package io.jtelegram.api.update;
 
 import io.jtelegram.api.TelegramBot;
 import io.jtelegram.api.events.Event;
+import io.jtelegram.api.events.location.LocationUpdateEvent;
 import io.jtelegram.api.events.message.MessageEvent;
+import io.jtelegram.api.events.message.edit.CaptionEditEvent;
+import io.jtelegram.api.events.message.edit.TextMessageEditEvent;
+import io.jtelegram.api.message.CaptionableMessage;
+import io.jtelegram.api.message.Message;
 import io.jtelegram.api.message.MessageType;
+import io.jtelegram.api.message.impl.LocationMessage;
+import io.jtelegram.api.message.impl.TextMessage;
 import io.jtelegram.api.requests.GetUpdates;
 import io.jtelegram.api.requests.framework.TelegramRequest;
+import io.jtelegram.api.update.types.EditedMessageUpdate;
 import io.jtelegram.api.update.types.MessageUpdate;
 import lombok.RequiredArgsConstructor;
 import okhttp3.Response;
@@ -50,6 +58,21 @@ public class PollingUpdateThread extends Thread {
             }
 
             return event;
+        });
+
+        EVENT_FUNCTIONS.put(EditedMessageUpdate.class, (bot, up) -> {
+            EditedMessageUpdate update = (EditedMessageUpdate) up;
+            Message updatedMessage = update.getEditedMessage();
+
+            if (updatedMessage instanceof TextMessage) {
+                return new TextMessageEditEvent(bot, (TextMessage) updatedMessage);
+            } else if (updatedMessage instanceof LocationMessage) {
+                return new LocationUpdateEvent(bot, (LocationMessage) updatedMessage);
+            } else if (updatedMessage instanceof CaptionableMessage) {
+                return new CaptionEditEvent(bot, (CaptionableMessage) updatedMessage);
+            }
+
+            return null;
         });
     }
 

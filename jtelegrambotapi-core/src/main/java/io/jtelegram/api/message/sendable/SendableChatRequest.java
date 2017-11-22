898a71783d3201a58931cbfc412f4e346ea29dd0
Amir Omidi <amir@aaomidi.com>
2017-11-22 11:55:35 -0500

    SendableChatRequest to stop repeating ChatId.

---
+++ jtelegrambotapi-core/src/main/java/io/jtelegram/api/message/sendable/SendableChatRequest.java
@@ -0,0 +1,23 @@
+package io.jtelegram.api.message.sendable;
+
+import io.jtelegram.api.chat.id.ChatId;
+import io.jtelegram.api.ex.TelegramException;
+import io.jtelegram.api.requests.framework.QueryTelegramRequest;
+import lombok.Getter;
+
+import java.util.function.Consumer;
+
+@Getter
+public class SendableChatRequest<T> extends QueryTelegramRequest<T> {
+    private final ChatId chatId;
+
+    protected SendableChatRequest(String endPoint, Class<T> callbackType, Consumer<T> callback, Consumer<TelegramException> errorHandler, ChatId chatId) {
+        super(endPoint, callbackType, callback, errorHandler);
+        this.chatId = chatId;
+    }
+
+    @Override
+    protected boolean isValid() {
+        return chatId != null;
+    }
+}

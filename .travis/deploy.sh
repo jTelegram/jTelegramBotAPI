529c7e5ead9b41dc4da1103f61229aa6514e09fc
Amir Omidi <amir@aaomidi.com>
2017-11-27 15:32:26 -0500

    Whooops.

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,35 +1,35 @@
-##!/bin/bash
-#
-#set -e
-#
-## only do deployment, when travis detects a new tag
-#if [ ! -z "$TRAVIS_TAG" ]
-#then
-#    echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
-#    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.3:set -DnewVersion=$TRAVIS_TAG -Prelease
-#
-#    if [ ! -z "$TRAVIS" -a -f "$HOME/.gnupg" ]; then
-#        shred -v ~/.gnupg/*
-#        rm -rf ~/.gnupg
-#    fi
-#
-#    source .travis/gpg.sh
-#
-#    mvn clean deploy --settings .travis/settings.xml -DskipTests=true --batch-mode --update-snapshots -Prelease
-#
-#
-#    if [ ! -z "$TRAVIS" ]; then
-#        shred -v ~/.gnupg/*
-#        rm -rf ~/.gnupg
-#    fi
-#else
-#    echo "not on a tag -> keep snapshot version in pom.xml"
-#    mvn clean deploy --settings .travis/settings.xml -DskipTests=true -B -U
-#fi
+#!/bin/bash
+
+set -e
+
+# only do deployment, when travis detects a new tag
+if [ ! -z "$TRAVIS_TAG" ]
+then
+    echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
+    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.3:set -DnewVersion=$TRAVIS_TAG -Prelease
+
+    if [ ! -z "$TRAVIS" -a -f "$HOME/.gnupg" ]; then
+        shred -v ~/.gnupg/*
+        rm -rf ~/.gnupg
+    fi
+
+    source .travis/gpg.sh
+
+    mvn clean deploy --settings .travis/settings.xml -DskipTests=true --batch-mode --update-snapshots -Prelease
+
+
+    if [ ! -z "$TRAVIS" ]; then
+        shred -v ~/.gnupg/*
+        rm -rf ~/.gnupg
+    fi
+else
+    echo "not on a tag -> keep snapshot version in pom.xml"
+    mvn clean deploy --settings .travis/settings.xml -DskipTests=true -B -U
+fi
 
 if [ ! -z "$SSH_KEY" ]
 then
-    mvn clean package
+    #mvn clean package
     # Setup deployment key
     echo $SSH_KEY | base64 --decode > $HOME/.ssh/id_rsa
     chmod 600 $HOME/.ssh/id_rsa
@@ -44,7 +44,7 @@ then
     git config user.name "Travis CI"
     git config user.email "travis@travis-ci.org"
 
-    cp ../apidocs/* .
+    cp -rf ../apidocs/* .
 
     git remote add javadoc
     git add --all

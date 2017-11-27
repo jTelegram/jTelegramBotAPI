bc85999a269a06e9a15358cb8834dc38eaa432e7
Amir Omidi <amir@aaomidi.com>
2017-11-26 23:38:08 -0500

    Update script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,9 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+
+# only do deployment, when travis detects a new tag
 if [ ! -z "$TRAVIS_TAG" ]
 then
     echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
-    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
+    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.3:set -DnewVersion=$TRAVIS_TAG -Prelease
+
+    if [ ! -z "$TRAVIS" -a -f "$HOME/.gnupg" ]; then
+        shred -v ~/.gnupg/*
+        rm -rf ~/.gnupg
+    fi
+
+    source .travis/gpg.sh
+
+    mvn clean deploy --settings .travis/settings.xml -DskipTests=true --batch-mode --update-snapshots -Prelease
+
+
+    if [ ! -z "$TRAVIS" ]; then
+        shred -v ~/.gnupg/*
+        rm -rf ~/.gnupg
+    fi
 else
     echo "not on a tag -> keep snapshot version in pom.xml"
 fi
-
-mvn clean deploy --settings .travis/settings.xml -DskipTests=true -B -U

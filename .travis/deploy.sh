b9f1a764d14609df4dcc6d4ace15482590c126d3
Amir Omidi <amir@aaomidi.com>
2017-11-27 14:42:11 -0500

    Whoops.

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,34 +1,34 @@
-#!/bin/bash
+##!/bin/bash
+#
+#set -e
+#
+## only do deployment, when travis detects a new tag
+#if [ ! -z "$TRAVIS_TAG" ]
+#then
+#    echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
+#    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.3:set -DnewVersion=$TRAVIS_TAG -Prelease
+#
+#    if [ ! -z "$TRAVIS" -a -f "$HOME/.gnupg" ]; then
+#        shred -v ~/.gnupg/*
+#        rm -rf ~/.gnupg
+#    fi
+#
+#    source .travis/gpg.sh
+#
+#    mvn clean deploy --settings .travis/settings.xml -DskipTests=true --batch-mode --update-snapshots -Prelease
+#
+#
+#    if [ ! -z "$TRAVIS" ]; then
+#        shred -v ~/.gnupg/*
+#        rm -rf ~/.gnupg
+#    fi
+#else
+#    echo "not on a tag -> keep snapshot version in pom.xml"
+#    mvn clean deploy --settings .travis/settings.xml -DskipTests=true -B -U
+#fi
 
-set -e
 
-# only do deployment, when travis detects a new tag
-if [ ! -z "$TRAVIS_TAG" ]
-then
-    echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
-    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.3:set -DnewVersion=$TRAVIS_TAG -Prelease
-
-    if [ ! -z "$TRAVIS" -a -f "$HOME/.gnupg" ]; then
-        shred -v ~/.gnupg/*
-        rm -rf ~/.gnupg
-    fi
-
-    source .travis/gpg.sh
-
-    mvn clean deploy --settings .travis/settings.xml -DskipTests=true --batch-mode --update-snapshots -Prelease
-
-
-    if [ ! -z "$TRAVIS" ]; then
-        shred -v ~/.gnupg/*
-        rm -rf ~/.gnupg
-    fi
-else
-    echo "not on a tag -> keep snapshot version in pom.xml"
-    mvn clean deploy --settings .travis/settings.xml -DskipTests=true -B -U
-fi
-
-
-cd jtelegramapi-core/target/apidocs
+cd jtelegrambotapi-core/target/apidocs
 git init
 git remote add javadoc git@github.com:jTelegram/jTelegramBotAPI.git
 

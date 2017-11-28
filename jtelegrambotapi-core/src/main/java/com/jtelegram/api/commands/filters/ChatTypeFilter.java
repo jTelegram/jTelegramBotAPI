17fb8a48fcfdcb8a6c0b601e9ce7117d5f57e91b
Nick Robson <robson.nicholas1@gmail.com>
2017-11-29 06:53:10 +1100

    Add RootFilter (groups filters) and ChatTypeFilter (filters by chat type)

---
+++ jtelegrambotapi-core/src/main/java/com/jtelegram/api/commands/filters/ChatTypeFilter.java
@@ -0,0 +1,51 @@
+package com.jtelegram.api.commands.filters;
+
+import com.jtelegram.api.chat.ChatType;
+import com.jtelegram.api.commands.Command;
+import com.jtelegram.api.events.message.TextMessageEvent;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.Set;
+
+/**
+ * A {@link CommandFilter} testing which type of chat the
+ * command was executed in.
+ *
+ * @author Nick Robson
+ */
+public class ChatTypeFilter extends CommandFilter {
+
+    private final Set<ChatType> chatTypes;
+
+    /**
+     * Creates a ChatTypeFilter with given children.
+     *
+     * @param chatType The ONLY type of chat to allow commands from
+     * @param children The children filters, which will be checked in order
+     *                 if this filter tests to be {@code true}
+     */
+    public ChatTypeFilter(ChatType chatType, CommandFilter... children) {
+        super(children);
+        this.chatTypes = Collections.singleton(chatType);
+    }
+
+    /**
+     * Creates a ChatTypeFilter with given children.
+     *
+     * @param chatTypes The types of chat to allow commands from
+     * @param children The children filters, which will be checked in order
+     *                 if this filter tests to be {@code true}
+     */
+    public ChatTypeFilter(ChatType[] chatTypes, CommandFilter... children) {
+        super(children);
+        this.chatTypes = EnumSet.copyOf(Arrays.asList(chatTypes));
+    }
+
+    @Override
+    protected boolean preTest(TextMessageEvent event, Command command) {
+        ChatType type = event.getMessage().getChat().getType();
+        return this.chatTypes.contains(type);
+    }
+
+}
---
+++ jtelegrambotapi-core/src/main/java/com/jtelegram/api/commands/filters/RootFilter.java
@@ -0,0 +1,35 @@
+package com.jtelegram.api.commands.filters;
+
+import com.jtelegram.api.commands.Command;
+import com.jtelegram.api.events.message.TextMessageEvent;
+
+/**
+ * A {@link CommandFilter} that merely groups filters.
+ *
+ * @author Nick Robson
+ */
+public class RootFilter extends CommandFilter {
+
+    private static CommandFilter[] combine(CommandFilter first, CommandFilter... remaining) {
+        CommandFilter[] filters = new CommandFilter[remaining.length + 1];
+        filters[0] = first;
+        System.arraycopy(remaining, 0, filters, 1, remaining.length);
+        return filters;
+    }
+
+    /**
+     * Creates a RootFilter with given children.
+     *
+     * @param filter The first filter
+     * @param children The remaining children filters, which will be checked in order
+     *                 if this filter tests to be {@code true}
+     */
+    public RootFilter(CommandFilter filter, CommandFilter... children) {
+        super(combine(filter, children));
+    }
+
+    @Override
+    protected boolean preTest(TextMessageEvent event, Command command) {
+        return false;
+    }
+}

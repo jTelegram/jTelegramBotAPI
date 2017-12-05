ae3d36be3e8daa70066f5418b82f48132fb1ac22
Nick Robson <robson.nicholas1@gmail.com>
2017-12-05 23:01:16 +1100

    Add getMessage to TelegramException so it's clear what the error was

--- jtelegrambotapi-core/src/main/java/com/jtelegram/api/ex/TelegramException.java
+++ jtelegrambotapi-core/src/main/java/com/jtelegram/api/ex/TelegramException.java
@@ -2,10 +2,17 @@ package com.jtelegram.api.ex;
 
 import com.jtelegram.api.requests.framework.ResponseParameters;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
 public class TelegramException extends Exception {
     private int errorCode;
     private String description;
     private ResponseParameters parameters;
+
+    @Override
+    public String getMessage() {
+        return this.toString();
+    }
 }

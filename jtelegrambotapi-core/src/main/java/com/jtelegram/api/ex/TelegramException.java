1e3d2a7f6665d2ee1645c1a716cd66a699518e6d
Nick Robson <robson.nicholas1@gmail.com>
2018-02-18 21:45:53 +1100

    Remove merge conflicts

--- jtelegrambotapi-core/src/main/java/com/jtelegram/api/ex/TelegramException.java
+++ jtelegrambotapi-core/src/main/java/com/jtelegram/api/ex/TelegramException.java
@@ -5,14 +5,8 @@ import lombok.Getter;
 import lombok.ToString;
 
 @Getter
-@ToString
 public class TelegramException extends Exception {
     private int errorCode;
     private String description;
     private ResponseParameters parameters;
-
-    @Override
-    public String getMessage() {
-        return this.toString();
-    }
 }

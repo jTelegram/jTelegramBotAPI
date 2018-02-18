4407a74bf9afbe7a417d2b98192341704f942643
Nick Robson <robson.nicholas1@gmail.com>
2018-02-18 21:47:05 +1100

    Remove unused import

--- jtelegrambotapi-core/src/main/java/com/jtelegram/api/ex/TelegramException.java
+++ jtelegrambotapi-core/src/main/java/com/jtelegram/api/ex/TelegramException.java
@@ -2,7 +2,6 @@ package com.jtelegram.api.ex;
 
 import com.jtelegram.api.requests.framework.ResponseParameters;
 import lombok.Getter;
-import lombok.ToString;
 
 @Getter
 public class TelegramException extends Exception {

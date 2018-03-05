42b6ce9025b47e6b6a99d91693eefe4af81346b2
Amir Omidi <amir@aaomidi.com>
2018-03-04 20:07:10 -0500

    viewers with builders.

--- jtelegrambotapi-menus/src/main/java/com/jtelegram/api/menu/viewer/InlineMenuViewer.java
+++ jtelegrambotapi-menus/src/main/java/com/jtelegram/api/menu/viewer/InlineMenuViewer.java
@@ -3,13 +3,19 @@ package com.jtelegram.api.menu.viewer;
 import com.jtelegram.api.menu.Menu;
 import com.jtelegram.api.menu.MenuViewer;
 import com.jtelegram.api.requests.message.edit.EditMessageReplyMarkup;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
+@ToString
 @Getter
-@AllArgsConstructor
 public class InlineMenuViewer implements MenuViewer {
     private String inlineMessageId;
+
+    @Builder
+    public InlineMenuViewer(String inlineMessageId) {
+        this.inlineMessageId = inlineMessageId;
+    }
 
     @Override
     public void sendMenu(Menu menu) {
--- jtelegrambotapi-menus/src/main/java/com/jtelegram/api/menu/viewer/RegularMenuViewer.java
+++ jtelegrambotapi-menus/src/main/java/com/jtelegram/api/menu/viewer/RegularMenuViewer.java
@@ -4,14 +4,21 @@ import com.jtelegram.api.chat.id.ChatId;
 import com.jtelegram.api.menu.Menu;
 import com.jtelegram.api.menu.MenuViewer;
 import com.jtelegram.api.requests.message.edit.EditMessageReplyMarkup;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
+@ToString
 @Getter
-@AllArgsConstructor
 public class RegularMenuViewer implements MenuViewer {
     private ChatId chatId;
     private int messageId;
+
+    @Builder
+    public RegularMenuViewer(ChatId chatId, int messageId) {
+        this.chatId = chatId;
+        this.messageId = messageId;
+    }
 
     @Override
     public void sendMenu(Menu menu) {

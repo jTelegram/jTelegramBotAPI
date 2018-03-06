e6802e455c307626f31be409fe07598765b6d624
Bo0tzz <btdewilligen@gmail.com>
2018-03-06 21:51:49 +0100

    Add missing lombok.Builder annotations to classes in api.requests package

--- jtelegrambotapi-core/src/main/java/com/jtelegram/api/requests/chat/GetChatAdministrators.java
+++ jtelegrambotapi-core/src/main/java/com/jtelegram/api/requests/chat/GetChatAdministrators.java
@@ -4,6 +4,7 @@ import com.jtelegram.api.chat.ChatMember;
 import com.jtelegram.api.chat.id.ChatId;
 import com.jtelegram.api.ex.TelegramException;
 import com.jtelegram.api.requests.framework.QueryTelegramRequest;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -14,6 +15,7 @@ import java.util.function.Consumer;
 public class GetChatAdministrators extends QueryTelegramRequest<ChatMember[]> {
     private ChatId chatId;
 
+    @Builder
     public GetChatAdministrators(Consumer<ChatMember[]> callback, Consumer<TelegramException> errorHandler, ChatId chatId) {
         super("getChatAdministrators", ChatMember[].class, callback, errorHandler);
         this.chatId = chatId;
--- jtelegrambotapi-core/src/main/java/com/jtelegram/api/requests/chat/admin/DeleteChatStickerSet.java
+++ jtelegrambotapi-core/src/main/java/com/jtelegram/api/requests/chat/admin/DeleteChatStickerSet.java
@@ -3,6 +3,7 @@ package com.jtelegram.api.requests.chat.admin;
 import com.jtelegram.api.chat.id.ChatId;
 import com.jtelegram.api.ex.TelegramException;
 import com.jtelegram.api.requests.framework.UpdateTelegramRequest;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -13,6 +14,7 @@ import java.util.function.Consumer;
 public class DeleteChatStickerSet extends UpdateTelegramRequest {
     private ChatId chatId;
 
+    @Builder
     public DeleteChatStickerSet(Consumer<TelegramException> errorHandler, Runnable callback, ChatId chatId) {
         super("deleteChatStickerSet", errorHandler, callback);
         this.chatId = chatId;

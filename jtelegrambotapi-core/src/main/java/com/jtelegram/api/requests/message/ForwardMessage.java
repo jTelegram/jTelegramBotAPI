f6515cde74eadaaf87aa96d3b826d31d17b10ca0
Nick Robson <robson.nicholas1@gmail.com>
2018-11-10 13:46:38 +1100

    Add methods to easily forward/delete messages

--- jtelegrambotapi-core/src/main/java/com/jtelegram/api/requests/message/ForwardMessage.java
+++ jtelegrambotapi-core/src/main/java/com/jtelegram/api/requests/message/ForwardMessage.java
@@ -5,14 +5,16 @@ import com.jtelegram.api.ex.TelegramException;
 import com.jtelegram.api.message.Message;
 import com.jtelegram.api.requests.message.framework.ReplyMarkup;
 import com.jtelegram.api.requests.message.framework.req.SendableMessageRequest;
+import java.util.Objects;
+import java.util.function.Consumer;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
 
-import java.util.function.Consumer;
-
-@ToString
 @Getter
+@ToString
+@EqualsAndHashCode(callSuper = true)
 public class ForwardMessage extends SendableMessageRequest<Message> {
     private final ChatId fromChatId;
     private final Integer messageId;
@@ -28,6 +30,20 @@ public class ForwardMessage extends SendableMessageRequest<Message> {
     @Override
     protected boolean isValid() {
         return super.isValid() && fromChatId != null && messageId != null;
+    }
+
+    /**
+     * Creates a request builder to forward the specified message.
+     *
+     * @param message the message that will be forwarded when the request is executed
+     *
+     * @return the request builder
+     */
+    public static ForwardMessageBuilder forMessage(Message<?> message) {
+        Objects.requireNonNull(message, "message cannot be null");
+        return builder()
+                .chatId(message.getChat().getChatId())
+                .messageId(message.getMessageId());
     }
 
 }

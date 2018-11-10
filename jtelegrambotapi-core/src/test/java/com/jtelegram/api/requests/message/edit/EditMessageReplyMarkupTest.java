2ae655736faac3f5e6134e982e8c16e3ccc6255b
Nick Robson <robson.nicholas1@gmail.com>
2018-11-10 13:47:10 +1100

    Add unit tests for editing markup, forwarding, and deleting of messages; fix edit location test

--- jtelegrambotapi-core/src/test/java/com/jtelegram/api/requests/message/edit/EditMessageReplyMarkupTest.java
+++ jtelegrambotapi-core/src/test/java/com/jtelegram/api/requests/message/edit/EditMessageReplyMarkupTest.java
@@ -3,7 +3,7 @@ package com.jtelegram.api.requests.message.edit;
 import com.jtelegram.api.chat.Chat;
 import com.jtelegram.api.chat.SupergroupChat;
 import com.jtelegram.api.chat.id.ChatId;
-import com.jtelegram.api.message.impl.LocationMessage;
+import com.jtelegram.api.message.Message;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -15,47 +15,47 @@ import static org.mockito.Mockito.when;
 /**
  * @author Nick Robson
  */
-public class EditMessageLiveLocationTest {
+public class EditMessageReplyMarkupTest {
 
     private static final long CHAT_MOCK_ID = 0xC0_FEFE;
     private static final int MESSAGE_MOCK_ID = 0xCAFE_CAB;
     private static final String INLINE_ID = "test_inline_id";
 
-    private static LocationMessage locationMessageMock;
+    private static Message messageMock;
 
     @BeforeAll
     public static void setup() {
         Chat chatMock = spy(SupergroupChat.class);
         when(chatMock.getChatId()).thenReturn(ChatId.of(CHAT_MOCK_ID));
 
-        locationMessageMock = spy(LocationMessage.class);
-        when(locationMessageMock.getChat()).thenReturn(chatMock);
-        when(locationMessageMock.getMessageId()).thenReturn(MESSAGE_MOCK_ID);
+        messageMock = spy(Message.class);
+        when(messageMock.getChat()).thenReturn(chatMock);
+        when(messageMock.getMessageId()).thenReturn(MESSAGE_MOCK_ID);
     }
 
     @Test
-    public void toEditLiveLocationRequest_output_same_as_forMessage() {
-        EditMessageLiveLocation expected = EditMessageLiveLocation.forMessage(locationMessageMock).build();
-        EditMessageLiveLocation actual = locationMessageMock.toEditLiveLocationRequest().build();
+    public void toEditTextRequest_output_same_as_forMessage() {
+        EditMessageReplyMarkup expected = EditMessageReplyMarkup.forMessage(messageMock).build();
+        EditMessageReplyMarkup actual = messageMock.toEditReplyMarkupRequest().build();
 
         assertEquals(expected, actual);
     }
 
     @Test
-    public void builds_with_correct_chatid_and_messageid() {
-        EditMessageLiveLocation request = locationMessageMock.toEditLiveLocationRequest().build();
+    public void builds_with_correct_chatid_and_messageid_text() {
+        EditMessageReplyMarkup request = messageMock.toEditReplyMarkupRequest().build();
 
         assertEquals(CHAT_MOCK_ID, request.getChatId().getId());
-        assertEquals(MESSAGE_MOCK_ID, request.getMessageId().intValue());
+        assertEquals(MESSAGE_MOCK_ID, request.getMessageId());
         assertNull(request.getInlineMessageId());
     }
 
     @Test
     public void builds_with_correct_inlineid() {
-        EditMessageLiveLocation request = EditMessageLiveLocation.forInlineMessage(INLINE_ID).build();
+        EditMessageReplyMarkup request = EditMessageReplyMarkup.forInlineMessage(INLINE_ID).build();
 
         assertNull(request.getChatId());
-        assertNull(request.getMessageId());
+        assertEquals(0, request.getMessageId());
         assertEquals(INLINE_ID, request.getInlineMessageId());
     }
 
